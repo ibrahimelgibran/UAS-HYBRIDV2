638ded0a43f49cd57a634cf856ca30a32a0f8026
rifanndi <rifandiannass@gmail.com>
2024-07-04 22:48:48 +0700

    update warna

--- src/app/profile/favorite/favorite.page.ts
+++ src/app/profile/favorite/favorite.page.ts
@@ -6,10 +6,13 @@ import { ProfileService } from 'src/app/services/profile.service';
   templateUrl: './favorite.page.html',
 })
 export class FavoritePage {
+  // Observable yang berisi daftar film favorit
   favoriteMovies$ = this.profileService.favoriteMovies$;
 
+  // Menginisialisasi ProfileService untuk mengambil data film favorit
   constructor(private profileService: ProfileService) {}
 
+  // Metode untuk kembali ke halaman sebelumnya
   goBack() {
     history.back();
   }
